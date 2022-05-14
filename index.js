bddce71ef3645ad876bac1751b914a212d83b494
unknown <russelhkamal@gmail.com>
2022-05-13 21:48:27 -0400

    created most of the constant variables in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const teamArr = [];
+//imports inquirer package
+const inquirer = require('inquirer');
+//imports classes
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const fs = require('fs');
+
