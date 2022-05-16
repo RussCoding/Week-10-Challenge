091c2c2e27bafae98263d1a61419e25c9fb9c233
unknown <russelhkamal@gmail.com>
2022-05-16 00:50:20 -0400

    finished html builder

--- src/htmlTemplate.js
+++ src/htmlTemplate.js
@@ -78,5 +78,25 @@ return htmlArr.join("");
 }
 
 module.exports = team => {
-    
+    return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css"/>
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <header>
+        <h1>Team Builder</h1>
+        </header>
+        <div class = "team-cards"> ${makeTeam(team)} </div>
+         
+    </body>
+    </html>
+
+    `;
+
 }
